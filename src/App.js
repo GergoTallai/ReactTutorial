eeff2e0fcee9be911ef9d16181c8c9eec6120878
Gergo MKV <gergo.tallai@gmail.com>
2022-07-19 10:15:40 +0200

    class, hook

--- src/App.js
+++ src/App.js
@@ -1,21 +1,100 @@
 import logo from './logo.svg';
 import Person from './Person/Person';
 import './App.css';
-import React from 'react';
+import React, {Component} from 'react'; //class használat
+//import React, {useState} from 'react'; //hook használat. UseState kezeli a függvényalapú komponenseket!
 /*
 Index.js-ben található a hivatkozás a renderelésre és az "APP"-ra.
 */
+
+/* HOOK HASZNÁLAT
+const App = props => {
+
+  const [aktualisState, ujState] = useState({ //Mindig két értéket ad vissza a UseState
+    //Speciális objektum a state, foglalt kulcsszó, ezzel adható meg értékek
+    persons: [
+      {name: "Balázs", age: "22"},
+      {name: "János", age: "32"},
+    ],
+    masikState: 'másik state'
+  });
+
+  //Kattintás kezelés
+  const nameChangeHander = ()=> {
+    //NE HASZNÁLD!!! this.state.persons[0].name = "Jakab"
+    ujState({
+      persons: [
+        {name: "Jakab", age: "99"},
+        {name: "Karcsi", age: "85"},
+      ]
+    }); 
+  }
+
+ return( //Mindig kell return
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>Party hard!</p>
+          <button onClick={nameChangeHander}>Katt ide</button> 
+          <Person name={aktualisState.persons[0].name} age={aktualisState.persons[0].age} />
+          <Person name={aktualisState.persons[1].name} age={aktualisState.persons[1].age}>Hobbim az info!</Person>
+        </header>
+      </div>
+      //Button Metódus neve utánra "()" raksz akkor betöltéskor lefut
+  );
+}
+*/
+
+/*
+Funkcióval is megoldható:
 function App() {
 
   return ( //Minidg kell return
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Party hard!</p>
-        <Person />
-      </header>
+    <div>
+    ...
     </div>
   );
+}
+*/
+
+//OSZTÁLY ALAPÚ:
+class App extends Component {
+
+  state = { //Speciális objektum a state, foglalt kulcsszó, ezzel adható meg értékek
+    persons: [
+      {name: "Balázs", age: "22"},
+      {name: "János", age: "32"},
+    ],
+    masikState: 'másik state'
+  }
+
+  nameChangeHander = ()=> {
+    //NE HASZNÁLD!!! this.state.persons[0].name = "Jakab"
+    this.setState({
+      persons: [
+        {name: "Jakab", age: "99"},
+        {name: "Karcsi", age: "85"},
+      ],
+      masikState: 'Másik state'
+    });
+    console.log(this.state);
+  }
+
+  render(){
+
+    return( //Mindig kell return
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>Party hard!</p>
+          <button onClick={this.nameChangeHander}>Katt ide</button> 
+          <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+          <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>Hobbim az info!</Person>
+        </header>
+      </div>
+      //Button Metódus neve utánra "()" raksz akkor betöltéskor lefut
+    );
+  }
 }
 
 export default App;
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
-const person = () => {
-    return <p>Üdvözlet Person komponensből! Random: {Math.floor(Math.random() * 6 + 1)}</p>
+const person = (props) => { //props bármi más lehet de íratlan szabály, ez kell ha a komponens kezel változót
+    return (
+        <div>
+            <p>Üdvözlet Person komponensből! Random: {Math.floor(Math.random() * 6 + 1)}, nevem: {props.name}, age {props.age}</p>
+            <p>{props.children}</p>
+        </div>
+    )
 }
 
 export default person;
